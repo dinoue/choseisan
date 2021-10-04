f97de7937b9c223e9eee6b787b08bd397a304283
Daisuke Inoue <inoue.daisuke@gmail.com>
2021-10-04 20:17:18 +0900

    added home page and devise pages.
    (cherry picked from commit 5cd3743e366a659c4a4428fbc3f2935cda211488)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  validates :login_id, length: { maximum: 10 }, presence: true, uniqueness: true
+  validates :password, length: { maximum: 10 }, presence: true
+  validates :name,     length: { maximum: 10 }, presence: true
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["login_id = :value", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
