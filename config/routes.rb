40ffd9da6046324c2c60efb0f475375b6b1ea996
Daisuke Inoue <inoue.daisuke@gmail.com>
2021-10-06 21:24:54 +0900

    added event-entry function.
    (cherry picked from commit cc6e7d531f527977e63042c29dc7471f2a12079e)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'entries/create'
-  get 'entries/update'
-  get 'entries/destroy'
-  resources :events
+  resources :events do
+    resource :entry, only: [:create, :update, :destroy]
+  end
   devise_for :views
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
