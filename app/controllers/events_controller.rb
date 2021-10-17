0672b971a1cdde26cbc2b9e69ce88073494de06d
Daisuke Inoue <inoue.daisuke@gmail.com>
2021-10-17 21:50:48 +0900

    improved generate too many select-query on event-show-view.
    (cherry picked from commit cfc268dcf460dd10984f64d1ba795d36d70bbdf9)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,7 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event.event_entries = @event.event_entries.eager_load(:user).order(id: :asc)
+    @event.event_entries = @event.event_entries.eager_load(:user, option_entries: :option).order(id: :asc)
   end
 
   def new
