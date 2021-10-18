c1f5282002ba08d8236a8bd760c02ed571531ba9
INOUE <inoue.daisuke@gmail.com>
2021-10-18 10:30:38 +0900

    event class 、status attribute のゴミ掃除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_203903) do
+ActiveRecord::Schema.define(version: 2021_10_07_192400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_10_07_203903) do
     t.bigint "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "status", default: 0, null: false
     t.index ["event_id"], name: "index_options_on_event_id"
   end
 
