517d11baf9a25870c190ca2f45b889a5855380e4
INOUE <inoue.daisuke@gmail.com>
2021-10-21 21:24:11 +0900

    クエリー生成方法を改善
    ログインユーザーを抽出条件に使用するものは、ログインユーザーからメソッドチェーンして生成するべき
    
    (cherry picked from commit dd4e15379b06dd56f6b2dd256280a5ce34c8487b)

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,7 @@ class User < ApplicationRecord
   validates :name,     length: { maximum: 10 }, presence: true
 
   has_many :created_events, class_name: 'Event', foreign_key: :user_id
-  has_many :event_entries, foreign_key: :event_id, dependent: :destroy
-
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["login_id = :value", { :value => login }]).first
-    else
-      where(conditions).first
-    end
-  end
+  has_many :event_entries, dependent: :destroy
 
   def related_events
     Event.where(Event.arel_table[:user_id].eq(self.id).or(EventEntry.arel_table[:user_id].eq(self.id)))
